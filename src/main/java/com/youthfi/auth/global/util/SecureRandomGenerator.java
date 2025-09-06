4b93326120b5af9f886e3f1661a673d048894f3a
ydking0911 <ydking0911@gmail.com>
2025-09-07 00:56:19 +0900

    [FEAT] Auth 서비스 구현
    - signup
    - login
    - email domain
    - jwt token
    - global setting

---
+++ src/main/java/com/youthfi/auth/global/util/SecureRandomGenerator.java
@@ -0,0 +1,19 @@
+package com.youthfi.auth.global.util;
+
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+public class SecureRandomGenerator {
+
+    private final SecureRandom secureRandom = new SecureRandom();
+
+    public String generate() {
+        int random = secureRandom.nextInt(1_000_000);
+        return String.format("%06d", random);
+    }
+
+}
+
+
